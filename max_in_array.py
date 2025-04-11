0328a683d880669a53910fd6d82bebd656a2fd12
Vineesh Chowdary <151981001+Vineeshchowdary6826@users.noreply.github.com>
2025-04-11 20:21:01 +0530

    Create max_in_array.py

---
+++ max_in_array.py
@@ -0,0 +1,10 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = [int(x) for x in input().split()]
+    maximum = arr[0]
+    for i in range(1, n):
+        if arr[i] > maximum:
+            maximum = arr[i]
+    
+    print(maximum)
